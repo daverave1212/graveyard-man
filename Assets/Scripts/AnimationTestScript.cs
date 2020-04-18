280bd4c24d9138aeea1d648cf8de5708e5bc3353
daverave1212 <36113284+daverave1212@users.noreply.github.com>
2020-04-18 20:33:26 +0300

    Added shovel and thief models. Made run animation for thief. It's pretty bad but it works

---
+++ Assets/Scripts/AnimationTestScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTestScript : MonoBehaviour
+{
+    void Start() {
+        GetComponent<Animator>().Play("Running");
+    }
+
+    void Update() {
+        
+    }
+}
