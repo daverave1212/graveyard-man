dd18fc07bcc6df0464b5ea8b44345ad2f8d6d735
daverave1212 <36113284+daverave1212@users.noreply.github.com>
2020-04-28 17:32:20 +0300

    Made the menu screen

--- Assets/Scripts/Slider.cs
+++ Assets/Scripts/Slider.cs
@@ -10,7 +10,7 @@ public class Slider : MonoBehaviour
     public Vector3 fromPoint;
     public Vector3 toPoint;
 
-    public float speed = 100.0f;
+    public float speed = 1.0f;
     public float startTime;
 
     public float journeyLength;
@@ -19,28 +19,28 @@ public class Slider : MonoBehaviour
 
     bool isSliding = false;
 
-    public void PauseSliding() { isSliding = false; }
-    public void ContinueSliding() { isSliding = true; }
-
-    public void SlideTo(Vector3 to, System.Action<int> callback) {
+    public void SlideTo(Vector3 to, System.Action<int> callback = null, float overTime = 1.0f) {
         fromPoint = gameObject.transform.position;
         toPoint = to;
+        print("Starting to slide from:");
+        print(fromPoint);
+        print("To");
+        print(toPoint);
         startTime = Time.time;
-        //journeyLength = Vector3.Distance(fromPoint, toPoint);
-        journeyLength = 1f;
+        journeyLength = overTime;
         isSliding = true;
         this.callback = callback;
     }
-
+    int _updateCount = 1;
     void Update() {
         if (!isSliding) return;
         float distCovered = (Time.time - startTime) * speed;
         float fractionOfJourney = distCovered / journeyLength;
-        print("Fraction of journey:" + fractionOfJourney);
+        print(_updateCount + " - Fraction of journey:" + fractionOfJourney);
         transform.position = Vector3.Lerp(fromPoint, toPoint, fractionOfJourney);
         if (fractionOfJourney >= 1) {
             isSliding = false;
-            callback(0);
+            if(callback != null) callback(0);
         }
     }
 }
